6f74bda0c85302cd1c1b5593122ba8cc3beb5035
sesp13 <sespinosarte99@gmail.com>
2022-06-13 15:35:05 -0500

    03-todoApp: Add basic component structure

--- 03-todoApp/src/app/app.module.ts
+++ 03-todoApp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosModule } from './todos/todos.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 03-todoApp/src/app/todos/todos.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+
+@NgModule({
+  declarations: [
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent,
+  ],
+  imports: [CommonModule],
+  exports: [TodoPageComponent],
+})
+export class TodosModule {}
