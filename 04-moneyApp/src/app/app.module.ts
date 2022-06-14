ad822b5352dbc7efb970d9476c5ee1dbcc67a8b7
sesp13 <sespinosarte99@gmail.com>
2022-06-14 12:23:45 -0500

    04-moneyApp: Init project and add basic structure

---
+++ 04-moneyApp/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeOutcomeComponent } from './income-outcome/income-outcome.component';
+import { StatsComponent } from './income-outcome/stats/stats.component';
+import { DetailComponent } from './income-outcome/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IncomeOutcomeComponent,
+    StatsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 04-moneyApp/src/app/income-outcome/income-outcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-income-outcome',
+  templateUrl: './income-outcome.component.html',
+  styleUrls: ['./income-outcome.component.scss']
+})
+export class IncomeOutcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
