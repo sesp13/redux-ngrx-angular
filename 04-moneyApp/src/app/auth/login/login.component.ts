499f2eacdd3750981a5ec3b5063b0f2b0c0f4612
sesp13 <sespinosarte99@gmail.com>
2022-06-16 01:07:22 -0500

    04-moneyApp: use isloading state property

--- 04-moneyApp/src/app/auth/login/login.component.ts
+++ 04-moneyApp/src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
@@ -6,56 +6,74 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 import { AuthService } from 'src/app/services/auth.service';
+import { isLoading, stopLoading } from 'src/app/shared/ui.actions';
 import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
+export class LoginComponent implements OnInit, OnDestroy {
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  isLoading: boolean = false;
+  uiSubscription!: Subscription;
 
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {}
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+    this.uiSubscription = this.store.select('ui').subscribe((ui) => {
+      this.isLoading = ui.isLoading;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.uiSubscription.unsubscribe();
   }
 
   getFormField(field: string): AbstractControl | null {
     return this.loginForm.get(field);
   }
 
-  login() {
+  login(): void {
     if (this.loginForm.invalid) return;
 
-    Swal.fire({
-      title: 'Loading',
-      didOpen: () => {
-        Swal.showLoading();
-      },
-    });
+    this.store.dispatch(isLoading());
+
+    // Swal.fire({
+    //   title: 'Loading',
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
 
     const { email, password } = this.loginForm.value;
     this.authService
       .loginUser(email, password)
       .then(() => {
-        Swal.close();
+        // Swal.close();
+        this.store.dispatch(stopLoading());
         this.router.navigate(['/']);
       })
-      .catch((err) =>
+      .catch((err) => {
+        this.store.dispatch(stopLoading());
         Swal.fire({
           title: 'Error!',
           text: err?.message,
           icon: 'error',
-        })
-      );
+        });
+      });
   }
 }
--- 04-moneyApp/src/app/auth/register/register.component.ts
+++ 04-moneyApp/src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
@@ -7,7 +7,11 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 import { AuthService } from 'src/app/services/auth.service';
+import { isLoading, stopLoading } from 'src/app/shared/ui.actions';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,45 +19,61 @@ import Swal from 'sweetalert2';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   registerForm: FormGroup = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
+  isLoading: boolean = false;
+  uiSubscription!: Subscription;
+
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.uiSubscription = this.store
+      .select('ui')
+      .subscribe((ui) => (this.isLoading = ui.isLoading));
+  }
 
-  createUser() {
+  ngOnDestroy(): void {
+    this.uiSubscription.unsubscribe();
+  }
+
+  createUser(): void {
     if (this.registerForm.invalid) return;
 
-    Swal.fire({
-      title: 'Loading',
-      didOpen: () => {
-        Swal.showLoading();
-      },
-    });
+    this.store.dispatch(isLoading());
+
+    // Swal.fire({
+    //   title: 'Loading',
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
 
     const { name, email, password } = this.registerForm.value;
     this.authService
       .createUser({ name, email, password })
       .then(() => {
-        Swal.close();
+        this.store.dispatch(stopLoading());
+        // Swal.close();
         this.router.navigate(['/']);
       })
-      .catch((err) =>
+      .catch((err) => {
+        this.store.dispatch(stopLoading());
         Swal.fire({
           title: 'Error!',
           text: err?.message,
           icon: 'error',
-        })
-      );
+        });
+      });
   }
 
   getFormField(field: string): AbstractControl | null {
