22d09fa56741204c27d0b407b518b2eade782f03
sesp13 <sespinosarte99@gmail.com>
2022-06-20 01:27:28 -0500

    05-effectsApp: Add basic project structure

--- 05-effectsApp/src/app/app.module.ts
+++ 05-effectsApp/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './users/users.module';
 
+// Components
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
